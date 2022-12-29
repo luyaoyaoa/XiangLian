7f5d10009cb5dc47bb8d39fc9e77494894db6e14
luyaoyaoa <you@536533550@qq.com>
2022-12-29 16:14:58 +0800

    初始化项目

---
+++ XiangLian/XiangLian/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XiangLian
+//
+//  Created by 袁路遥 on 2022/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
